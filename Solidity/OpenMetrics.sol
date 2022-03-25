062ce92cc234e535dbf557958086b37dc171c6cb
Albert Garuda <algarud@gmail.com>
2022-03-25 21:26:59 +0200

    Init solidity

---
+++ Solidity/OpenMetrics.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract OpenMetrics {
+    address payable public owner;
+
+    mapping(uint256 => Metric) public Metrics;
+    uint256 public articlesCount;
+
+    struct Metric {
+        address creator;
+        address approver;
+        Status status;
+    }
+
+    enum Status {
+        None,
+        Approved
+    }
+
+    event Log(address indexed sender, uint256 metricId);
+
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function widthraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function submitArticle() public {
+        
+    }
+
+    function editArticle() public {
+
+    }
+}

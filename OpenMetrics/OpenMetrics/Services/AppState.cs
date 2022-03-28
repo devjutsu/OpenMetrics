7510dff556c8649812407b5f494594e06ef11e59
Albert Garuda <algarud@gmail.com>
2022-03-28 22:37:40 +0300

    App state & login

---
+++ OpenMetrics/OpenMetrics/Services/AppState.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace OpenMetrics.Services
+{
+    public class AppState
+    {
+        public long ChainId { get; private set; }
+        public string AccountId {get; private set;}
+        public bool IsAuthenticated => AccountId != null && !string.IsNullOrWhiteSpace(AccountId);
+
+        public string AccountToShow()
+        {
+            if ((string.IsNullOrEmpty(AccountId)))
+                return string.Empty;
+            else
+                return $"{AccountId.Substring(0, 6)}...{AccountId.Substring(AccountId.Length - 4)}";
+        }
+
+        private IJSRuntime _js;
+
+        public AppState(IJSRuntime JS)
+        {
+            _js = JS;
+        }
+
+        public void SetChain(ComponentBase source, long chainId)
+        {
+            this.ChainId = chainId;
+            NotifyStateChanged(source, "ChainId");
+        }
+
+        public async Task Login(ComponentBase source, string address)
+        {
+            NotifyStateChanged(source, "CurrentUser");
+        }
+
+        public event Action<ComponentBase, string> Statechanged;
+        private void NotifyStateChanged(ComponentBase source, string Property) =>
+            Statechanged?.Invoke(source, Property);
+    }
+}

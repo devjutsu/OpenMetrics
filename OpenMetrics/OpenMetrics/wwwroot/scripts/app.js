f0c59be1433346e1d73b883d39a7eb691c8ecf24
Albert Garuda <algarud@gmail.com>
2022-03-28 23:54:29 +0300

    chain switch call

---
+++ OpenMetrics/OpenMetrics/wwwroot/scripts/app.js
@@ -0,0 +1,7 @@
+﻿window.switchChain = async function (chainId) {
+    console.log('switch chain called');
+    await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: chainId }],
+    });
+}

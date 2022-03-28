4e26648cddb39bb630bc9f55628a732d08f36e49
Albert Garuda <algarud@gmail.com>
2022-03-28 23:11:07 +0300

    Toast, AppState

---
+++ OpenMetrics/OpenMetrics/ClientConfig.cs
@@ -0,0 +1,26 @@
+﻿namespace OpenMetrics
+{
+    public class ClientConfig
+    {
+        public string ApiUrl { get; set; }
+        public long NetworkId { get; set; }
+
+        //public string ContractAddress { get; set; }
+        //public string RpcUrl { get; set; }
+        //public string ChainId { get; set; }
+    }
+
+    public static class NetworksList
+    {
+        public static Dictionary<long, string> Networks = new Dictionary<long, string>
+        {
+            { 0x1, "Mainnet" },
+            { 0x3, "Ropsten" },
+            { 0x4, "Rinkeby" },
+            { 0x61, "BSC Testnet"},
+            { 0x38, "BSC Mainnet"},
+            { 0x89, "Polygon Mainnet"},
+            { 0x13881, "Polygon Mumbai Testnet"}
+        };
+    }
+}
--- OpenMetrics/OpenMetrics/Program.cs
+++ OpenMetrics/OpenMetrics/Program.cs
@@ -1,7 +1,11 @@
-using MetaMask.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MetaMask.Blazor;
+using Blazored.Toast;
 using OpenMetrics;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OpenMetrics.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +13,17 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+long netId = 1;
+long.TryParse(builder.Configuration["NetworkId"], out netId);
+var config = new ClientConfig()
+{
+    ApiUrl = builder.Configuration["ApiUrl"] ?? "",
+    NetworkId = netId,
+};
+
+builder.Services.AddSingleton<ClientConfig>(config);
 builder.Services.AddMetaMaskBlazor();
+builder.Services.AddBlazoredToast();
+builder.Services.AddScoped<AppState>();
 
 await builder.Build().RunAsync();

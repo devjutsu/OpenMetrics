b809b45670f3f2a02c30c9569a11bbf41540ce3f
Albert Garuda <algarud@gmail.com>
2022-03-29 23:45:31 +0300

    Tune metrics model

--- OpenMetrics/OpenMetrics/ViewModels/Metric.cs
+++ OpenMetrics/OpenMetrics/ViewModels/Metric.cs
@@ -3,6 +3,11 @@
     public class Metric
     {
         public int Id { get; set; }
-        public string Text { get; set; }
+        public string Cid { get; set; }
+        public string Creator { get; set; }
+        public string Editor { get; set; }
+        public string Approver { get; set; }
+        public int Status { get; set; }
+        public byte[] Checksum { get; set; }
     }
 }

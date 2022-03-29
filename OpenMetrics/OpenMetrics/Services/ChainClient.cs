e9584ee144504de9026772092c0f7aa844fe8747
Albert Garuda <algarud@gmail.com>
2022-03-29 21:50:29 +0300

    Chain client inject dependencies

--- OpenMetrics/OpenMetrics/Services/ChainClient.cs
+++ OpenMetrics/OpenMetrics/Services/ChainClient.cs
@@ -2,16 +2,28 @@
 using Nethereum.JsonRpc.Client;
 using Nethereum.Web3.Accounts;
 using Nethereum.Contracts;
+using Blazored.Toast.Services;
 
 namespace OpenMetrics.Services
 {
     public interface IChain
     {
-
+        //Task<bool> GetData();
     }
 
     public class ChainClient : IChain
     {
+        private readonly AppState _state;
+        private readonly IToastService _toast;
+        private readonly ClientConfig _config;
 
+        public ChainClient(AppState state, IToastService toast, ClientConfig config)
+        {
+            _state = state;
+            _toast = toast;
+            _config = config;
+        }
+
+        
     }
 }

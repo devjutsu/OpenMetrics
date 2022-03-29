20577b7351f627d5c0cc9095fcb7d611fbc01385
Albert Garuda <algarud@gmail.com>
2022-03-29 21:47:32 +0300

    Init chain client

---
+++ OpenMetrics/OpenMetrics/Services/ChainClient.cs
@@ -0,0 +1,17 @@
+﻿using Nethereum.Web3;
+using Nethereum.JsonRpc.Client;
+using Nethereum.Web3.Accounts;
+using Nethereum.Contracts;
+
+namespace OpenMetrics.Services
+{
+    public interface IChain
+    {
+
+    }
+
+    public class ChainClient : IChain
+    {
+
+    }
+}
